b821c7acb4acafd78518edb8a4b75843d456ba03
MartinHenriksen <martin.henriksen@burohappold.com>
2019-07-02 13:21:28 +0200

    added Delete methods and type conversion

---
+++ RFEM_Engine/Convert/ToRFEM/Type.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BH.oM.Structure.Elements;
+using BH.oM.Structure.Constraints;
+using rf = Dlubal.RFEM5;
+
+namespace BH.Engine.RFEM
+{
+    public static partial class Convert
+    {
+        /***************************************************/
+        /**** Public Methods                            ****/
+        /***************************************************/
+
+        //Add methods for converting From BHoM to the specific software types, if possible to do without any BHoM calls
+
+        public static rf.ModelObjectType ToRfemType(this Type bhObjectType)
+        {
+            string typeString = bhObjectType.ToString();
+
+            switch (typeString)
+            {
+                case "Node":
+                    return rf.ModelObjectType.NodeObject;
+                case "Bar":
+                    return rf.ModelObjectType.MemberObject;
+                case "Material":
+                    return rf.ModelObjectType.MaterialObject;
+                case "SectionProperty":
+                    return rf.ModelObjectType.CrossSectionObject;
+                default:
+                    return rf.ModelObjectType.UnknownObject;
+            }
+        }
+    }
+}
