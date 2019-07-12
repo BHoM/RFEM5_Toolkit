9dc4397549036d809a7b5274b38162ccce8d5ae3
MartinHenriksen <martin.henriksen@burohappold.com>
2019-07-12 09:27:04 +0200

    convert bar.ToRFEM

---
+++ RFEM_Engine/Convert/ToRFEM/Bar.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BH.oM.Physical.Materials;
+using BH.oM.Structure.Elements;
+using BH.oM.Structure.SectionProperties;
+using rf = Dlubal.RFEM5;
+
+namespace BH.Engine.RFEM
+{
+    public static partial class Convert
+    {
+        /***************************************************/
+        /**** Public Methods                            ****/
+        /***************************************************/
+
+        public static rf.Member ToRFEM(this Bar bar, int barId, int lineId)
+        {
+            rf.Member rfBar = new rf.Member();
+            rfBar.No = barId;
+            rfBar.LineNo = lineId;
+
+            rfBar.StartCrossSectionNo = System.Convert.ToInt32(bar.SectionProperty.CustomData[AdapterId]);
+
+            return rfBar;
+        }
+
+
+    }
+}
