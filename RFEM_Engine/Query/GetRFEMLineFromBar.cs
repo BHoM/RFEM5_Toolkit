ee0f9d5826d0e484be76a7c1dcafdfa8557827be
MartinHenriksen <martin.henriksen@burohappold.com>
2019-07-17 17:28:00 +0200

    moved creation of bar centreline to a query method

---
+++ RFEM_Engine/Query/GetRFEMLineFromBar.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BH.oM.Structure.Elements;
+using BH.oM.Structure.Constraints;
+using BH.Engine.RFEM;
+using rf = Dlubal.RFEM5;
+
+namespace BH.Engine.RFEM
+{
+    public static partial class Query
+    {
+        /***************************************************/
+        /**** Public Methods                            ****/
+        /***************************************************/
+
+  
+        public static rf.Line GetRFEMLineFromBar(Bar bar, int lineId)
+        {
+            rf.Line centreLine = new rf.Line();
+            centreLine.No = lineId;
+            int startNodeId = System.Convert.ToInt32(bar.StartNode.CustomData[Convert.AdapterId]);
+            int endNodeId = System.Convert.ToInt32(bar.EndNode.CustomData[Convert.AdapterId]);
+            centreLine.NodeList = String.Join(",", new int[] { startNodeId, endNodeId });
+            centreLine.Type = rf.LineType.PolylineType;
+
+            return centreLine;
+        }
+
+        /***************************************************/
+    }
+}
