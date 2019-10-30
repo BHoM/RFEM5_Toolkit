10cecd8fd8fdb26b121a3706cba7a1f2ba0110ad
Martin Henriksen <martinhenriksen83@gmail.com>
2019-10-30 18:37:08 +0100

    remove Config settings

--- RFEM_Adapter/RFEMAdapter.cs
+++ RFEM_Adapter/RFEMAdapter.cs
@@ -57,11 +57,7 @@ namespace BH.Adapter.RFEM
             {
                 AdapterId = BH.Engine.RFEM.Convert.AdapterId;   //Set the "AdapterId" to "SoftwareName_id". Generally stored as a constant string in the convert class in the SoftwareName_Engine
 
-                Config.SeparateProperties = true;   //Set to true to push dependant properties of objects before the main objects are being pushed. Example: push nodes before pushing bars
-                Config.MergeWithComparer = true;    //Set to true to use EqualityComparers to merge objects. Example: merge nodes in the same location
-                Config.ProcessInMemory = false;     //Set to false to to update objects in the toolkit during the push
                 Config.CloneBeforePush = false;      //Set to true to clone the objects before they are being pushed through the software. Required if any modifications at all, as adding a software ID is done to the objects
-                Config.UseAdapterId = true;         //Tag objects with a software specific id in the CustomData. Requires the NextIndex method to be overridden and implemented
 
 
                 if (!IsApplicationRunning())
