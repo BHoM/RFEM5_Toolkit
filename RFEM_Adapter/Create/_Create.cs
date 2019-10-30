2c672c872b531c3f2a7f488f3c87296ca5eece6e
Martin Henriksen <martinhenriksen83@gmail.com>
2019-10-30 18:33:42 +0100

    remove ReplaceAll from Create method

--- RFEM_Adapter/Create/_Create.cs
+++ RFEM_Adapter/Create/_Create.cs
@@ -39,7 +39,7 @@ namespace BH.Adapter.RFEM
         /**** Adapter overload method                   ****/
         /***************************************************/
 
-        protected override bool Create<T>(IEnumerable<T> objects, bool replaceAll = false)
+        protected override bool Create<T>(IEnumerable<T> objects)
         {
             bool success = true;
 
