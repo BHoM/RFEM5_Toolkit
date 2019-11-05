1420f2241f3f8e5e6fbbf9ecdc182242da37843e
Martin Henriksen <martinhenriksen83@gmail.com>
2019-11-05 15:50:17 +0100

    add NuGet package reference

--- RFEM_Adapter/RFEMAdapter.cs
+++ RFEM_Adapter/RFEMAdapter.cs
@@ -29,7 +29,7 @@ using BH.oM.Base;
 using BH.oM.Data.Requests;
 using System.Runtime.InteropServices;
 using rf = Dlubal.RFEM5;
-using Dlubal.RFEM5; //I am going to remove this
+using Dlubal.RFEM5; 
 
 using System.Reflection;
 using System.Diagnostics;
