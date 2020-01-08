aa0afccab7cac9539c92e27a73726a072899513e
MartinHenriksen <martin.henriksen@burohappold.com>
2020-01-08 16:35:10 +0100

    add rfemConfig

---
+++ RFEM_oM/RFEMConfig.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BH.oM.Adapter;
+
+namespace BH.oM.Adapter.RFEM
+{
+    public class RFEMConfig : ActionConfig
+    {
+        public bool Replace {get;set;}
+        public bool CloneBeforePush { get; set; } = false; //TODO: check if this was made obsolete in previous adapterRefactoring
+        public bool Is2DModel { get; set; } = false;
+        public bool ZIsUp { get; set; } = true;
+    }
+}
