0b7838a1032f7740ed378fa61b637114d41a021c
MartinHenriksen <martin.henriksen@burohappold.com>
2020-01-08 13:53:55 +0100

    move pull override to seperate file

---
+++ RFEM_Adapter/AdapterActions/Pull.cs
@@ -0,0 +1,59 @@
+﻿/*
+ * This file is part of the Buildings and Habitats object Model (BHoM)
+ * Copyright (c) 2015 - 2019, the respective contributors. All rights reserved.
+ *
+ * Each contributor holds copyright over their respective contributions.
+ * The project versioning (Git) records all such contribution source information.
+ *                                           
+ *                                                                              
+ * The BHoM is free software: you can redistribute it and/or modify         
+ * it under the terms of the GNU Lesser General Public License as published by  
+ * the Free Software Foundation, either version 3.0 of the License, or          
+ * (at your option) any later version.                                          
+ *                                                                              
+ * The BHoM is distributed in the hope that it will be useful,              
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of               
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the                 
+ * GNU Lesser General Public License for more details.                          
+ *                                                                            
+ * You should have received a copy of the GNU Lesser General Public License     
+ * along with this code. If not, see <https://www.gnu.org/licenses/lgpl-3.0.html>.      
+ */
+
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BH.oM.Common.Materials;
+using BH.oM.Structure.Elements;
+using BH.oM.Base;
+using BH.oM.Data.Requests;
+using System.Runtime.InteropServices;
+using rf = Dlubal.RFEM5;
+using Dlubal.RFEM5;
+
+using System.Reflection;
+using System.Diagnostics;
+using System;
+using System.IO;
+using System.Collections.Generic;
+using BH.oM.Structure.SectionProperties;
+using BH.oM.Structure.MaterialFragments;
+
+namespace BH.Adapter.RFEM
+{
+    public partial class RFEMAdapter : BHoMAdapter
+    {
+        public override IEnumerable<object> Pull(IRequest request, Dictionary<string, object> config = null)
+        {
+            if (IsApplicationRunning() & TryToShowApp())
+                return base.Pull(request, config);
+            else
+            {
+                BH.Engine.Reflection.Compute.RecordError("Make sure that either the RFEM Adapter component has opened an instance of the application or you have opened one yourself."
+               + "\nCheck if you have a frozen instance of RFEM in the background. Look in Task Manager.");
+                return null;
+            }
+        }
+
+    }
+}
