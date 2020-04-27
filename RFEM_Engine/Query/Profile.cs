5e7b3105f8e21716047ccf60478dbb6683ec19cf
Martin Henriksen <martinhenriksen83@gmail.com>
2020-04-27 17:31:48 +0200

    seperate switch for standard and parametric profiles in convert.FromRFEM

--- RFEM_Engine/Query/Profile.cs
+++ RFEM_Engine/Query/Profile.cs
@@ -48,6 +48,7 @@ namespace BH.Engine.RFEM
             string[] profileNameArr = profileName.Split(' ');
             string[] profileValues;
             double v1, v2, v3, v4, v5, v6, v7, v8, v9, v10;
+            IProfile profile = null;
 
             if (profileNameArr.Length > 1)
             {
@@ -74,100 +75,160 @@ namespace BH.Engine.RFEM
                     case "UB":
                     case "UBP":
                     case "UC":
-                        double h = sectionDBProps.FirstOrDefault(x => x.ID == rf3.DB_CRSC_PROPERTY_ID.CRSC_PROP_h).fValue;
-                        double w = sectionDBProps.FirstOrDefault(x => x.ID == rf3.DB_CRSC_PROPERTY_ID.CRSC_PROP_b).fValue;
-                        double wt = sectionDBProps.FirstOrDefault(x => x.ID == rf3.DB_CRSC_PROPERTY_ID.CRSC_PROP_t_s).fValue;
-                        double ft = sectionDBProps.FirstOrDefault(x => x.ID == rf3.DB_CRSC_PROPERTY_ID.CRSC_PROP_t_g).fValue;
-                        double fr = sectionDBProps.FirstOrDefault(x => x.ID == rf3.DB_CRSC_PROPERTY_ID.CRSC_PROP_r).fValue;
-                        double tr = sectionDBProps.FirstOrDefault(x => x.ID == rf3.DB_CRSC_PROPERTY_ID.CRSC_PROP_r_1).fValue;
-                        ISectionProfile profile_I = Structure.Create.ISectionProfile(h, w, wt, ft, fr, tr);
-                        return profile_I;
+                        v1 = sectionDBProps.FirstOrDefault(x => x.ID == rf3.DB_CRSC_PROPERTY_ID.CRSC_PROP_h).fValue;
+                        v2 = sectionDBProps.FirstOrDefault(x => x.ID == rf3.DB_CRSC_PROPERTY_ID.CRSC_PROP_b).fValue;
+                        v3 = sectionDBProps.FirstOrDefault(x => x.ID == rf3.DB_CRSC_PROPERTY_ID.CRSC_PROP_t_s).fValue;
+                        v4 = sectionDBProps.FirstOrDefault(x => x.ID == rf3.DB_CRSC_PROPERTY_ID.CRSC_PROP_t_g).fValue;
+                        v5 = sectionDBProps.FirstOrDefault(x => x.ID == rf3.DB_CRSC_PROPERTY_ID.CRSC_PROP_r).fValue;
+                        v6 = sectionDBProps.FirstOrDefault(x => x.ID == rf3.DB_CRSC_PROPERTY_ID.CRSC_PROP_r_1).fValue;
+                        profile = Structure.Create.ISectionProfile(v1,v2,v3,v4,v5,v6);
+                        break;
+                    case "SHS":
+                    case "RHS":
+                    case "QRO":
+                    case "RRO":
+                        v1 = sectionDBProps.FirstOrDefault(x => x.ID == rf3.DB_CRSC_PROPERTY_ID.CRSC_PROP_h).fValue;
+                        v2 = sectionDBProps.FirstOrDefault(x => x.ID == rf3.DB_CRSC_PROPERTY_ID.CRSC_PROP_b).fValue;
+                        v3 = sectionDBProps.FirstOrDefault(x => x.ID == rf3.DB_CRSC_PROPERTY_ID.CRSC_PROP_s).fValue;
+                        v4 = sectionDBProps.FirstOrDefault(x => x.ID == rf3.DB_CRSC_PROPERTY_ID.CRSC_PROP_r).fValue;
+                        v5 = sectionDBProps.FirstOrDefault(x => x.ID == rf3.DB_CRSC_PROPERTY_ID.CRSC_PROP_r_1).fValue;
+                        profile = Structure.Create.BoxProfile(v1, v2, v3, v4, v5);
+                        break;
+                    case "SQ-S":
+                    case "SQ-R":
+                    case "4KT":
+                        v1 = sectionDBProps.FirstOrDefault(x => x.ID == rf3.DB_CRSC_PROPERTY_ID.CRSC_PROP_h).fValue;
+                        v2 = sectionDBProps.FirstOrDefault(x => x.ID == rf3.DB_CRSC_PROPERTY_ID.CRSC_PROP_b).fValue;
+                        v3 = sectionDBProps.FirstOrDefault(x => x.ID == rf3.DB_CRSC_PROPERTY_ID.CRSC_PROP_r).fValue;
+                        profile = Structure.Create.RectangleProfile(v1, v2, v3);
+                        break;
+
+                    case "L":
+                    case "UKA":
+                    case "LS":
+                        v1 = sectionDBProps.FirstOrDefault(x => x.ID == rf3.DB_CRSC_PROPERTY_ID.CRSC_PROP_h).fValue;
+                        v2 = sectionDBProps.FirstOrDefault(x => x.ID == rf3.DB_CRSC_PROPERTY_ID.CRSC_PROP_b).fValue;
+                        v3 = sectionDBProps.FirstOrDefault(x => x.ID == rf3.DB_CRSC_PROPERTY_ID.CRSC_PROP_t_s).fValue;
+                        v4 = sectionDBProps.FirstOrDefault(x => x.ID == rf3.DB_CRSC_PROPERTY_ID.CRSC_PROP_t_g).fValue;
+                        v5 = sectionDBProps.FirstOrDefault(x => x.ID == rf3.DB_CRSC_PROPERTY_ID.CRSC_PROP_r).fValue;
+                        v6 = sectionDBProps.FirstOrDefault(x => x.ID == rf3.DB_CRSC_PROPERTY_ID.CRSC_PROP_r_1).fValue;
+                        profile = Structure.Create.AngleProfile(v1, v2, v3, v4, v5, v6);
+                        break;
+                    case "T":
+                    case "TB":
+                    case "TPS":
+                    case "TS":
+                        v1 = sectionDBProps.FirstOrDefault(x => x.ID == rf3.DB_CRSC_PROPERTY_ID.CRSC_PROP_h).fValue;
+                        v2 = sectionDBProps.FirstOrDefault(x => x.ID == rf3.DB_CRSC_PROPERTY_ID.CRSC_PROP_b).fValue;
+                        v3 = sectionDBProps.FirstOrDefault(x => x.ID == rf3.DB_CRSC_PROPERTY_ID.CRSC_PROP_t_s).fValue;
+                        v4 = sectionDBProps.FirstOrDefault(x => x.ID == rf3.DB_CRSC_PROPERTY_ID.CRSC_PROP_t_g).fValue;
+                        v5 = sectionDBProps.FirstOrDefault(x => x.ID == rf3.DB_CRSC_PROPERTY_ID.CRSC_PROP_r).fValue;
+                        v6 = sectionDBProps.FirstOrDefault(x => x.ID == rf3.DB_CRSC_PROPERTY_ID.CRSC_PROP_r_1).fValue;
+                        profile = Structure.Create.TSectionProfile(v1, v2, v3, v4, v5, v6);
+                        break;
+                    case "U":
+                    case "C":
+                    case "UPE":
+                    case "UAP":
+                    case "CH":
+                    case "UPN":
+                    case "PFC":
+                        v1 = sectionDBProps.FirstOrDefault(x => x.ID == rf3.DB_CRSC_PROPERTY_ID.CRSC_PROP_h).fValue;
+                        v2 = sectionDBProps.FirstOrDefault(x => x.ID == rf3.DB_CRSC_PROPERTY_ID.CRSC_PROP_b).fValue;
+                        v3 = sectionDBProps.FirstOrDefault(x => x.ID == rf3.DB_CRSC_PROPERTY_ID.CRSC_PROP_t_s).fValue;
+                        v4 = sectionDBProps.FirstOrDefault(x => x.ID == rf3.DB_CRSC_PROPERTY_ID.CRSC_PROP_t_g).fValue;
+                        v5 = sectionDBProps.FirstOrDefault(x => x.ID == rf3.DB_CRSC_PROPERTY_ID.CRSC_PROP_r).fValue;
+                        v6 = sectionDBProps.FirstOrDefault(x => x.ID == rf3.DB_CRSC_PROPERTY_ID.CRSC_PROP_r_1).fValue;
+                        profile = Structure.Create.ChannelProfile(v1, v2, v3, v4, v5, v6);
+                        break;
+                    case "CHS":
+                    case "RO":
+                        v1 = sectionDBProps.FirstOrDefault(x => x.ID == rf3.DB_CRSC_PROPERTY_ID.CRSC_PROP_r).fValue * 2;
+                        v2 = sectionDBProps.FirstOrDefault(x => x.ID == rf3.DB_CRSC_PROPERTY_ID.CRSC_PROP_s).fValue;
+                        profile = Structure.Create.TubeProfile(v1, v2);
+                        break;
+                    case "RD":
+                    case "ROD":
+                    case "RB":
+                        v1 = sectionDBProps.FirstOrDefault(x => x.ID == rf3.DB_CRSC_PROPERTY_ID.CRSC_PROP_r).fValue * 2;
+                        profile = Structure.Create.CircleProfile(v1);
+                        break;
+                    //case "Z":
+                    //case "KZ":
+                    //    v1 = sectionDBProps.FirstOrDefault(x => x.ID == rf3.DB_CRSC_PROPERTY_ID.CRSC_PROP_h).fValue;
+                    //    v2 = sectionDBProps.FirstOrDefault(x => x.ID == rf3.DB_CRSC_PROPERTY_ID.CRSC_PROP_b).fValue;
+                    //    v3 = sectionDBProps.FirstOrDefault(x => x.ID == rf3.DB_CRSC_PROPERTY_ID.CRSC_PROP_t_s).fValue;
+                    //    v4 = sectionDBProps.FirstOrDefault(x => x.ID == rf3.DB_CRSC_PROPERTY_ID.CRSC_PROP_t_g).fValue;
+                    //    v5 = sectionDBProps.FirstOrDefault(x => x.ID == rf3.DB_CRSC_PROPERTY_ID.CRSC_PROP_r).fValue;
+                    //    v6 = sectionDBProps.FirstOrDefault(x => x.ID == rf3.DB_CRSC_PROPERTY_ID.CRSC_PROP_r_1).fValue;
+                    //    profile = new ZSectionProfile(v1, v2, v3, v4, v5, v6, "< looks like the only way to create this now is to use the profile curves !! >");
+                    //    break;
                     default:
+                        Engine.Reflection.Compute.RecordError("Don't know how to make" + profileName);
                         break;
                 }
             }
 
-
-            switch (profileNameArr[0])
-            {
-                case "Rectangle":
-                case "T-Rectangle"://timber
-                    v1 = Convert.ToDouble(profileValues[0]);
-                    v2 = Convert.ToDouble(profileValues[1]);
-                    
-                    RectangleProfile profile = Structure.Create.RectangleProfile(v1, v2, 0);
-                    return profile;
-                case "SHS":
-                case "RHS":
-                case "QRO":
-                case "RRO":
-                case "TO":
-                case "HSH":
-                case "HSV":
-                    v1 = Convert.ToDouble(profileValues[0]);
-                    v2 = Convert.ToDouble(profileValues[1]);
-                    v3 = Convert.ToDouble(profileValues[2]);
-                    BoxProfile profile = Structure.Create.BoxProfile(v1, v2, v3, 0, 0);
-                    return profile;
-                case "L":
-                case "LU":
-                case "KLU":
-                case "LS":
-                    AngleProfile profile = new AngleProfile();
-                    break;
-                case "IS":
-                case "ITS":
-                case "IUH":
-                case "IUV":
-                    ISectionProfile profile = new ISectionProfile();
-                    break;
-                case "T":
-                case "TB":
-                case "TPS":
-                case "TS":
-                case "FB":
-                case "TH":
-                case "TV":
-                    TSectionProfile profile = new TSectionProfile();
-                    break;
-                case "U":
-                case "UPE":
-                case "UAP":
-                case "CH":
-                case "UPN":
-                case "PFC":
-                case "C":
-                case "UU":
-                case "UM":
-                case "PIH":
-                case "PIV":
-                    ChannelProfile profile = new ChannelProfile();
-                    break;
-                case "CHS":
-                case "RO":
-                case "Pipe":
-                case "Ring":
-                    TubeProfile profile = new TubeProfile();
-                    break;
-                case "RD":
-                case "ROD":
-                case "RB":
-                case "Round":
-                case "Circle":
-                case "T-Circle":
-                    CircleProfile profile = new CircleProfile();
-                    break;
-                case "Z":
-                case "KZ":
-                case "Z(A)":
-                case "Z(B)":
-                case "Z_AM":
-                case "Z_BM":
-                    ZSectionProfile profile = new ZSectionProfile();
-                    break;
-                default:
-                    break;
-            }
+            // for parametric section profiles
+            //switch (profileNameArr[0])
+            //{
+            //    case "Rectangle":
+            //    case "T-Rectangle"://timber
+            //        v1 = Convert.ToDouble(profileValues[0]);
+            //        v2 = Convert.ToDouble(profileValues[1]);
+            //        profile = Structure.Create.RectangleProfile(v1, v2, 0);
+            //        break;
+            //    case "TO":
+            //    case "HSH":
+            //    case "HSV":
+            //        v1 = Convert.ToDouble(profileValues[0]);
+            //        v2 = Convert.ToDouble(profileValues[1]);
+            //        v3 = Convert.ToDouble(profileValues[2]);
+            //        profile = Structure.Create.BoxProfile(v1, v2, v3, 0, 0);
+            //        break;
+            //    case "L":
+            //    case "LU":
+            //    case "KLU":
+            //    case "LS":
+            //        profile = new AngleProfile();
+            //        break;
+            //    case "IS":
+            //    case "ITS":
+            //    case "IUH":
+            //    case "IUV":
+            //        profile = new ISectionProfile();
+            //        break;
+            //    case "TS":
+            //    case "FB":
+            //    case "TH":
+            //    case "TV":
+            //        profile = new TSectionProfile();
+            //        break;
+            //    case "C":
+            //    case "UU":
+            //    case "UM":
+            //    case "PIH":
+            //    case "PIV":
+            //        profile = new ChannelProfile();
+            //        break;
+            //    case "Pipe":
+            //    case "Ring":
+            //        profile = new TubeProfile();
+            //        break;
+            //    case "Round":
+            //    case "Circle":
+            //    case "T-Circle":
+            //        profile = new CircleProfile();
+            //        break;
+            //    case "Z(A)":
+            //    case "Z(B)":
+            //    case "Z_AM":
+            //    case "Z_BM":
+            //        profile = new ZSectionProfile();
+            //        break;
+            //    default:
+            //        break;
+            //}
 
             /* ---TODO: cannot find shape types for these profiles: 
                 BH.oM.Geometry.ShapeProfiles.FabricatedBoxProfile
@@ -178,6 +239,7 @@ namespace BH.Engine.RFEM
                 BH.oM.Geometry.ShapeProfiles.TaperedProfile
                 */
 
+            return profile;
         }
 
 
