2d19a56a42e18de3e18b090d7c90800c70e48bd9
Martin Henriksen <martinhenriksen83@gmail.com>
2021-04-13 19:12:17 +0200

    read tapered section - steel only

--- RFEM_Adapter/CRUD/Read/Bar.cs
+++ RFEM_Adapter/CRUD/Read/Bar.cs
@@ -29,7 +29,7 @@ using System.Threading.Tasks;
 using BH.oM.Structure.Elements;
 using BH.oM.Structure.SectionProperties;
 using BH.oM.Structure.Constraints;
-
+using BH.Engine.Spatial;
 using rf = Dlubal.RFEM5;
 using BH.oM.Structure.MaterialFragments;
 
@@ -66,6 +66,41 @@ namespace BH.Adapter.RFEM
                         sectionProperty = rfISection.FromRFEM(rfMat);
                         m_sectionDict.Add(member.StartCrossSectionNo, sectionProperty);
                     }
+
+                    //check for tapered section
+                    if(member.EndCrossSectionNo!=0)
+                    {
+                        ISectionProperty startSectionProperty = sectionProperty;
+                        ISectionProperty endSectionProperty;
+
+
+                        if (!m_sectionDict.TryGetValue(member.EndCrossSectionNo, out endSectionProperty))
+                        {
+                            rf.ICrossSection rfISection = modelData.GetCrossSection(member.EndCrossSectionNo, rf.ItemAt.AtNo);
+                            rf.CrossSection rfSection = rfISection.GetData();
+                            rf.Material rfMat = modelData.GetMaterial(rfSection.MaterialNo, rf.ItemAt.AtNo).GetData();
+                            endSectionProperty = rfISection.FromRFEM(rfMat);
+                            m_sectionDict.Add(member.EndCrossSectionNo, endSectionProperty);
+                        }
+
+                        //check if section mixex material !!!
+                        if(startSectionProperty.Material.Name != endSectionProperty.Material.Name)//campare by name as comparing materials seems to always be not equal
+                            Engine.Reflection.Compute.RecordWarning("Tapered section mixes materials. Material from start of section is used");
+
+                        oM.Spatial.ShapeProfiles.IProfile taperProfile = null;
+
+                        if (startSectionProperty.Material.GetType() == typeof(Steel))
+                        {
+                            SteelSection startSection = startSectionProperty as SteelSection;
+                            SteelSection endSection = endSectionProperty as SteelSection;
+                            taperProfile = BH.Engine.Spatial.Create.TaperedProfile(startSection.SectionProfile, endSection.SectionProfile);
+                            taperProfile.Name = "TaperedProfile-" + startSection.Name + "-To-" + endSection.Name;
+                        }
+
+                        sectionProperty = BH.Engine.Structure.Create.SectionPropertyFromProfile(taperProfile, startSectionProperty.Material);
+
+                    }
+
 
                     barList.Add(member.FromRFEM(line, sectionProperty));
                 }
