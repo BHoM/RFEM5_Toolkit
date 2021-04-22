86926a97d94e1ca0a6bc568540871382d90f21ab
Martin Henriksen <martinhenriksen83@gmail.com>
2021-04-22 13:49:10 +0200

    initial Bar uniform load class

---
+++ RFEM_Adapter/Convert/ToRFEM/Load/BarUniformlyDistributed.cs
@@ -0,0 +1,59 @@
+﻿/*
+ * This file is part of the Buildings and Habitats object Model (BHoM)
+ * Copyright (c) 2015 - 2021, the respective contributors. All rights reserved.
+ *
+ * Each contributor holds copyright over their respective contributions.
+ * The project versioning (Git) records all such contribution source information.
+ *                                           
+ *                                                                              
+ * The BHoM is free software: you can redistribute it and/or modify         
+ * it under the terms of the GNU Lesser General Public License as published by  
+ * the Free Software Foundation, either version 3.0 of the License, or          
+ * (at your option) any later version.                                          
+ *                                                                              
+ * The BHoM is distributed in the hope that it will be useful,              
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of               
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the                 
+ * GNU Lesser General Public License for more details.                          
+ *                                                                            
+ * You should have received a copy of the GNU Lesser General Public License     
+ * along with this code. If not, see <https://www.gnu.org/licenses/lgpl-3.0.html>.      
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BH.oM.Physical.Materials;
+using BH.oM.Structure.Elements;
+using BH.oM.Structure.Loads;
+using BH.oM.Structure.SectionProperties;
+using BH.Engine.Adapter;
+using BH.oM.Adapters.RFEM;
+using rf = Dlubal.RFEM5;
+
+namespace BH.Adapter.RFEM
+{
+    public static partial class Convert
+    {
+        /***************************************************/
+        /**** Public Methods                            ****/
+        /***************************************************/
+
+        public static void ToRFEM(this BarUniformlyDistributedLoad load, int loadId)
+        {
+            rf.MemberLoad rfLoad = new rf.MemberLoad();
+
+            rfLoad.No = loadId;
+
+            //check if force, moment or both
+            rfLoad.Type = rf.LoadType.ForceType;
+            //rfLoad.Type = rf.LoadType.MomentType;
+            //rfLoad.Type = rf.LoadType.TemperatureType;
+            //rfLoad.Magnitude1 = load.Force;
+
+            //rfLoad.ObjectList = load.Objects.AdapterIds(typeof(RFEMId)).ToString();
+
+        }
+    }
