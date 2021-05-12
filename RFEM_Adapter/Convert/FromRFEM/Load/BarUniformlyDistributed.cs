86a995f2657844eed70833b724cf1da5e7cbdda4
Martin Henriksen <martinhenriksen83@gmail.com>
2021-05-12 14:01:50 +0200

    get loading param method

--- RFEM_Adapter/Convert/FromRFEM/Load/BarUniformlyDistributed.cs
+++ RFEM_Adapter/Convert/FromRFEM/Load/BarUniformlyDistributed.cs
@@ -44,23 +44,97 @@ namespace BH.Adapter.RFEM
 
         public static BarUniformlyDistributedLoad FromRFEM(this rf.MemberLoad rfMemberLoad, Loadcase bhLoadcase, oM.Base.BHoMGroup<Bar> objectGroup)
         {
+            LoadAxis loadAxis;
+            bool projected;
+            string direction;
+
             BarUniformlyDistributedLoad barUniformLoad = new BarUniformlyDistributedLoad();
+            barUniformLoad.Loadcase = bhLoadcase;
+            barUniformLoad.Objects = objectGroup;
 
-            if (rfMemberLoad.Direction == rf.LoadDirectionType.GlobalZType)
+            
+            GetLoadingParameters(rfMemberLoad, out loadAxis, out projected, out direction);
+            barUniformLoad.Axis = loadAxis;
+            barUniformLoad.Projected = projected;
+
+            switch (direction)
             {
-                barUniformLoad.Axis = LoadAxis.Global;
-                barUniformLoad.Force.Z = rfMemberLoad.Magnitude1;
-                barUniformLoad.Projected = false;
-                barUniformLoad.Loadcase = bhLoadcase;
-                barUniformLoad.Objects = objectGroup;
-
-                
+                case "X":
+                    barUniformLoad.Force.X = rfMemberLoad.Magnitude1;
+                    break;
+                case "Y":
+                    barUniformLoad.Force.Y = rfMemberLoad.Magnitude1;
+                    break;
+                case "Z":
+                    barUniformLoad.Force.Z = rfMemberLoad.Magnitude1;
+                    break;
+                default:
+                    break;
             }
+
 
             return barUniformLoad;
         }
 
         /***************************************************/
+
+        private static void GetLoadingParameters(rf.MemberLoad rfMemberLoad, out LoadAxis loadAxis, out bool projected, out string direction)
+        {
+            loadAxis = LoadAxis.Global;
+            projected = false;
+            direction = "";
+
+            switch (rfMemberLoad.Direction)
+            {
+                case rf.LoadDirectionType.GlobalXType:
+                    loadAxis = LoadAxis.Global;
+                    direction = "X";
+                    break;
+                case rf.LoadDirectionType.GlobalYType:
+                    loadAxis = LoadAxis.Global;
+                    direction = "Y";
+                    break;
+                case rf.LoadDirectionType.GlobalZType:
+                    loadAxis = LoadAxis.Global;
+                    direction = "Z";
+                    break;
+                case rf.LoadDirectionType.LocalXType:
+                    loadAxis = LoadAxis.Local;
+                    direction = "X";
+                    break;
+                case rf.LoadDirectionType.LocalYType:
+                    loadAxis = LoadAxis.Local;
+                    direction = "Y";
+                    break;
+                case rf.LoadDirectionType.LocalZType:
+                    loadAxis = LoadAxis.Local;
+                    direction = "Z";
+                    break;
+                case rf.LoadDirectionType.ProjectXType:
+                    loadAxis = LoadAxis.Global;
+                    projected = true;
+                    direction = "X";
+                    break;
+                case rf.LoadDirectionType.ProjectYType:
+                    loadAxis = LoadAxis.Global;
+                    projected = true;
+                    direction = "Y";
+                    break;
+                case rf.LoadDirectionType.ProjectZType:
+                    loadAxis = LoadAxis.Global;
+                    projected = true;
+                    direction = "Z";
+                    break;
+                case rf.LoadDirectionType.PerpendicularZType:
+                case rf.LoadDirectionType.UnknownLoadDirectionType:
+                case rf.LoadDirectionType.LocalUType:
+                case rf.LoadDirectionType.LocalVType:
+                    Engine.Reflection.Compute.RecordWarning("Load direction type: " + rfMemberLoad.Direction.ToString() + " is not supported!");
+                    break;
+                default:
+                    break;
+            }
+        }
     }
 }
 
