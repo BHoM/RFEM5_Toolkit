a1e0b428786b978afe88e6923d5ed2e649c812c1
Martin Henriksen <martinhenriksen83@gmail.com>
2021-05-12 11:29:53 +0200

    convert from RFEM for bar uniform load

---
+++ RFEM_Adapter/Convert/FromRFEM/Load/BarUniformlyDistributed.cs
@@ -0,0 +1,66 @@
+﻿/*
+ * This file is part of the Buildings and Habitats object Model (BHoM)
+ * Copyright (c) 2015 - 2021, the respective contributors. All rights reserved.
+ *
+ * Each contributor holds copyright over their respective contributions.
+ * The project versioning (Git) records all such contribution source information.
+ *                                           
+ *                                                                              
+ * The BHoM is free software: you can redistribute it and/or modify         
+ * it under the terms of the GNU Lesser General Public License as published by  
+ * the Free Software Foundation, either version 3.0 of the License, or          
+ * (at your option) any later version.                                          
+ *                                                                              
+ * The BHoM is distributed in the hope that it will be useful,              
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of               
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the                 
+ * GNU Lesser General Public License for more details.                          
+ *                                                                            
+ * You should have received a copy of the GNU Lesser General Public License     
+ * along with this code. If not, see <https://www.gnu.org/licenses/lgpl-3.0.html>.      
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BH.oM.Structure.Elements;
+using BH.oM.Structure.SectionProperties;
+using BH.oM.Structure.Constraints;
+using BH.oM.Structure.Loads;
+using BH.Adapter.RFEM;
+using BH.Engine.Adapter;
+using BH.oM.Adapters.RFEM;
+using rf = Dlubal.RFEM5;
+
+namespace BH.Adapter.RFEM
+{
+    public static partial class Convert
+    {
+        /***************************************************/
+        /**** Public Methods                            ****/
+        /***************************************************/
+
+        public static BarUniformlyDistributedLoad FromRFEM(this rf.MemberLoad rfMemberLoad, Loadcase bhLoadcase, oM.Base.BHoMGroup<Bar> objectGroup)
+        {
+            BarUniformlyDistributedLoad barUniformLoad = new BarUniformlyDistributedLoad();
+
+            if (rfMemberLoad.Direction == rf.LoadDirectionType.GlobalZType)
+            {
+                barUniformLoad.Axis = LoadAxis.Global;
+                barUniformLoad.Force.Z = rfMemberLoad.Magnitude1;
+                barUniformLoad.Projected = false;
+                barUniformLoad.Loadcase = bhLoadcase;
+                barUniformLoad.Objects = objectGroup;
+
+                
+            }
+
+            return barUniformLoad;
+        }
+
+        /***************************************************/
+    }
+}
+
