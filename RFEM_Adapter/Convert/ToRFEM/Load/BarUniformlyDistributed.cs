9cbd269be31f14f16d538cb5bc1d2010a85e6aee
MartinHenriksen <martin.henriksen@burohappold.com>
2021-09-29 13:51:50 +0200

    revert to individual load creation

--- RFEM_Adapter/Convert/ToRFEM/Load/BarUniformlyDistributed.cs
+++ RFEM_Adapter/Convert/ToRFEM/Load/BarUniformlyDistributed.cs
@@ -47,7 +47,7 @@ namespace BH.Adapter.RFEM
             List<rf.MemberLoad> rfLoadList = new List<rf.MemberLoad>();
 
             rf.MemberLoad rfLoad = new rf.MemberLoad();
-            rfLoad.No = loadId+7;//tmp for debug only
+            rfLoad.No = loadId;
             rfLoad.Distribution = rf.LoadDistributionType.UniformType;
             rfLoad.RelativeDistances = false;
             rfLoad.ReferenceTo = rf.MemberLoadReferenceType.MembersType;
@@ -56,12 +56,10 @@ namespace BH.Adapter.RFEM
             string objectIdString = string.Join(",", objectIds);
             rfLoad.ObjectList = objectIdString;
 
-            /*
             rfLoad.IsValid = true;
             rfLoad.Comment = "";
             rfLoad.ID = "";
             rfLoad.Tag = "";
-            */
 
             if(load.Force.Length() == 0 & load.Moment.Length() == 0)
             {
@@ -95,7 +93,6 @@ namespace BH.Adapter.RFEM
                     zLoad.Magnitude1 = load.Force.Z;
                     rfLoadList.Add(zLoad);
                 }
-
             }
 
             if (load.Moment.Length() != 0)
