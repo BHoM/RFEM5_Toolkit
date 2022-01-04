0efeb97dfd9f83fc5e780737950c8ec08ff85825
BHoMBot <BHoMBot@burohappold.com>
2022-01-04 14:14:33 +0000

    Update AssemblyFileVersion to 5.1.0.0

--- RFEM_oM/Properties/AssemblyInfo.cs
+++ RFEM_oM/Properties/AssemblyInfo.cs
@@ -56,6 +56,6 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 
 [assembly: AssemblyVersion("5.0.0.0")]
-[assembly: AssemblyFileVersion("5.0.0.0")]
+[assembly: AssemblyFileVersion("5.1.0.0")]
 
 
