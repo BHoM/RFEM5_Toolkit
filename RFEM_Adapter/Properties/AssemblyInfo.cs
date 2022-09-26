84c8aec3d862c283785a08bdb576425c95f97d6b
BHoMBot <BHoMBot@burohappold.com>
2022-09-26 23:58:46 +0100

    Update assembly file version to 6.0

--- RFEM_Adapter/Properties/AssemblyInfo.cs
+++ RFEM_Adapter/Properties/AssemblyInfo.cs
@@ -56,8 +56,8 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 
 
-[assembly: AssemblyVersion("5.0.0.0")]
-[assembly: AssemblyFileVersion("5.3.0.0")]
+[assembly: AssemblyVersion("6.0.0.0")]
+[assembly: AssemblyFileVersion("6.0.0.0")]
 
 
 
