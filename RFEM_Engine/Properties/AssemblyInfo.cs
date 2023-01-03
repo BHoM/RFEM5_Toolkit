954ee66b621aed4c1f5e83f333858b28584daef0
BHoMBot <BHoMBot@burohappold.com>
2023-01-03 11:45:56 +0000

    Upgrade AssemblyFileVersion to 6.1.0.0

--- RFEM_Engine/Properties/AssemblyInfo.cs
+++ RFEM_Engine/Properties/AssemblyInfo.cs
@@ -56,7 +56,7 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 
 [assembly: AssemblyVersion("6.0.0.0")]
-[assembly: AssemblyFileVersion("6.0.0.0")]
+[assembly: AssemblyFileVersion("6.1.0.0")]
 
 
 
